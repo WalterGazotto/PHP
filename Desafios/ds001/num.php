ba92471c52c53e146d774efda014fb3179abff64
Walter Gazotto <126080076+WalterGazotto@users.noreply.github.com>
2024-03-26 11:16:30 -0300

    Desafio 01 antecessor e sucessor

---
+++ Desafios/ds001/num.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Antecessor e Sucessor</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+    <h1>ResultadoFinal</h1>
+        <?php 
+            $valor = $_GET["numero"];
+            $valormaior = $valor + 1;
+            $valormenor = $valor - 1;
+
+            echo "O nuémero escolhido foi <strong>$valor</strong> <br>O seu antecessor é $valormenor <br>O seu sucessor é $valormaior";
+
+            echo "<br>"
+        ?>
+        <a href="index.html">Voltar</a>
+    </main>
+</body>
+</html>
